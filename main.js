71dc7ce720ce9adc96179ba36505e59454a5e5e6
Daniel Cardenas <97079553+darcardenasp045@users.noreply.github.com>
2023-09-11 18:50:50 -0500

    Update code to make chess pieces draggable.

--- main.js
+++ main.js
@@ -1,50 +1,60 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+
+    function moverFichas(fichas, deltaX, deltaY) {
+        for (let i = 0; i < fichas.length; i++) {
+            fichas[i].addEventListener('click', function () {
+                const element = fichas[i];
+    
+                // Obtiene la posición actual en X e Y del elemento
+                const currentX = element.offsetLeft;
+                const currentY = element.offsetTop;
+    
+                // Calcula las nuevas coordenadas sumando deltaX y deltaY
+                const newX = currentX + deltaX;
+                const newY = currentY + deltaY;
+    
+                // Mueve el elemento a las nuevas coordenadas
+                element.style.left = newX + 'px';
+                element.style.top = newY + 'px';
+    
+                console.log("Click en una ficha");
+            });
+        }
+    }
+    
+    // Ejemplo de uso para mover peones blancos 1 píxel hacia la derecha y 90 píxeles hacia abajo
     const whitePawns = document.getElementsByClassName('whitePawn');
-
-    for (let i = 0; i < whitePawns.length; i++) {
-        whitePawns[i].addEventListener('click', function () {
-            const element = whitePawns[i];
+    moverFichas(whitePawns, 1, 90);
     
-            // Obtiene la posición actual en X e Y del elemento
-            const currentX = element.offsetLeft;
-            const currentY = element.offsetTop;
+    // Ejemplo de uso para mover alfiles blancos 1 píxel hacia la derecha y 1 píxel hacia abajo
+    const whiteBishops = document.getElementsByClassName('whiteBishop');
+    moverFichas(whiteBishops, 1, 90);
     
-            // Calcula las nuevas coordenadas sumando 10px
-            const newX = currentX + 1;
-            const newY = currentY + 90;
-    
-            // Mueve el elemento a las nuevas coordenadas
-            element.style.left = newX + 'px';
-            element.style.top = newY + 'px';
-    
-            console.log("Click en Peón Blanco");
-        });
-    }
     
     
     
 
-    const whiteBishop = document.getElementsByClassName('whiteBishop')
-    for (let i = 0; i < whiteBishop.length; i++) {
-        whiteBishop[i].addEventListener("click", function () {
+    
+    // for (let i = 0; i < whiteBishop.length; i++) {
+    //     whiteBishop[i].addEventListener("click", function () {
             
-        })
-    }
+    //     })
+    // }
 
-    const whiteHorse = document.getElementsByClassName('whiteHorse')
-    for (let i = 0; i < whiteHorse.length; i++) {
-        whiteHorse[i].addEventListener("click", function () {
-            console.log("Click en caballo Blanco");
-        })
-    }
+    // const whiteHorse = document.getElementsByClassName('whiteHorse')
+    // for (let i = 0; i < whiteHorse.length; i++) {
+    //     whiteHorse[i].addEventListener("click", function () {
+    //         console.log("Click en caballo Blanco");
+    //     })
+    // }
 
-    const whiteTower = document.getElementsByClassName('whiteTower')
-    for (let i = 0; i < whiteTower.length; i++) {
-        whiteTower[i].addEventListener("click", function () {
-            console.log("Click en Torre Blanca");
-        })
-    }
+    // const whiteTower = document.getElementsByClassName('whiteTower')
+    // for (let i = 0; i < whiteTower.length; i++) {
+    //     whiteTower[i].addEventListener("click", function () {
+    //         console.log("Click en Torre Blanca");
+    //     })
+    // }
 
 
 
